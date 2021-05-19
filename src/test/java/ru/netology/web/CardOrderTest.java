3fb45b04be8fcd2b11c2f2abd08d6c3beda55d28
Aleksandr Osipov <eltisprim@mail.ru>
2021-05-19 16:33:43 -0400

    Validation check

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -19,4 +19,54 @@ public class CardOrderTest {
         $("[type='button']").click();
         $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldEmptyFiledName() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79801234567");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldEmptyFiledPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Александр Сергеевич Осипов");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldEmptyCheckBox() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Александр Сергеевич Осипов");
+        $("[data-test-id=phone] input").setValue("+79801234567");
+        //$("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='agreement']").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    void shouldNotValidName() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Aleksandr");
+        $("[data-test-id=phone] input").setValue("+79801234567");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotValidPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Александр Сергеевич Осипов");
+        $("[data-test-id=phone] input").setValue("+7980123456");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 }
